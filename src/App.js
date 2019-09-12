cc935dada67d20e6d943cb060390b3507ef175ae
dani690e <dani690e@videndjurs.net>
2019-09-12 09:21:02 +0200

    state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    citater: [
+      {id: 1, title: "Øje for et øje", citattext: "Et øje for et øje, det ender med at hele verden bliver blind.", forfatter: "Gandhi"},
+      {id: 2, title: "Don't let go", citattext: "I’ll never let go, Jack. I’ll never let go.", forfatter: "Rose"},
+      {id: 1, title: "Survive", citattext: "Promise me you’ll survive. That you won’t give up, no matter what happens. No matter how hopeless.", forfatter: "Jack"},
+    ]
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
