b37314882d2809d28533f0792f5d52b2fc17a656
dani690e <dani690e@videndjurs.net>
2019-09-12 13:41:00 +0200

    Change input value

--- src/AddCitater.js
+++ src/AddCitater.js
@@ -14,17 +14,22 @@ class AddCitater extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addCitat(this.state);
+        this.setState({
+            title: '',
+            citatText: '',
+            forfatter: '',
+        })
     }
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" onChange={this.handleChange}/>
+                    <input type="text" id="title" onChange={this.handleChange} value={this.state.title}/>
                     <label htmlFor="citatText">citatText:</label>
-                    <input type="text" id="citatText" onChange={this.handleChange}/>
+                    <input type="text" id="citatText" onChange={this.handleChange} value={this.state.citatText}/>
                     <label htmlFor="forfatter">Forfatter:</label>
-                    <input type="text" id="forfatter" onChange={this.handleChange}/>
+                    <input type="text" id="forfatter" onChange={this.handleChange} value={this.state.forfatter}/>
                     <button>Submit</button>
                 </form>
             </div>
